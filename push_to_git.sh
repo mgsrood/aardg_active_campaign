93887263f2dbad80e7132ae3a2968a9f2f894d9c
Maxvanaardg <max@aardg.nl>
2024-06-06 15:09:06 +0200

    Update 2024-06-06 15:09:06

--- push_to_git.sh
+++ push_to_git.sh
@@ -6,9 +6,6 @@ git add .
 # Commit the changes with a timestamp
 git commit -m "Update $(date +"%Y-%m-%d %H:%M:%S")"
 
-# Connect to the GitHub repository
-git remote add origin https://github.com/Maxvanaardg/active_campaign.git
-
 # Push the changes to the remote repository
 git push origin main
 
